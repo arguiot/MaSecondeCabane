8fa411f997fb5bed4abd6fe1e8b933fad08c678e
Arthur Guiot <arguiot@gmail.com>
2020-10-05 13:58:36 -0400

    Fixed issue with older browser

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,41 @@
 import '../styles/globals.scss'
-import { GeistProvider, CssBaseline, useTheme } from '@geist-ui/react'
+import {
+	GeistProvider,
+	CssBaseline,
+	useTheme
+} from '@geist-ui/react'
 import NextNProgress from 'nextjs-progressbar';
-import { Provider } from 'next-auth/client'
+import {
+	Provider
+} from 'next-auth/client'
 
-function MyApp({ Component, pageProps }) {
-  const [themeType, setThemeType] = React.useState('light')
-  React.useEffect(() => {
-    const md = window.matchMedia('(prefers-color-scheme: dark)')
-    setThemeType(lastThemeType => (md.matches ? 'dark' : 'light'))
-    
-    md.addEventListener('change', event => {
-      setThemeType(lastThemeType => (event.matches ? 'dark' : 'light'))
-    })
-  }, [])
+function MyApp({
+	Component,
+	pageProps
+}) {
+	const [themeType, setThemeType] = React.useState('light')
+	React.useEffect(() => {
+		const md = window.matchMedia('(prefers-color-scheme: dark)')
+		setThemeType(lastThemeType => (md.matches ? 'dark' : 'light'))
 
-  const theme = useTheme()
+		const handler = event => {
+			setThemeType(lastThemeType => (event.matches ? 'dark' : 'light'))
+		}
+
+		try {
+			// Chrome & Firefox
+			md.addEventListener('change', handler);
+		} catch (e1) {
+			try {
+				// Safari
+				md.addListener(handler);
+			} catch (e2) {
+				console.error(e2);
+			}
+		}
+	}, [])
+
+	const theme = useTheme()
 
   return (
     <Provider session={pageProps.session}>
