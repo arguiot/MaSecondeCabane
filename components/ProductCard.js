8cae7520318aea12e85a26584c29e120748c692f
Arthur Guiot <arguiot@gmail.com>
2020-10-20 13:27:43 -0400

    Trying green text in product card
    Signed-off-by: Arthur Guiot <arguiot@gmail.com>

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -6,7 +6,7 @@ export default function ProductCard({ product }) {
         <Card width="300" hoverable shadow style={{ cursor: "pointer", textAlign: "center" }}>
             <Image width={ 300 } height={ 200 } src={ `https://ik.imagekit.io/ittx2e0v7x/tr:w-300,h-200,fo-auto/${product.image}` } style={{ objectFit: 'cover' }} alt={ product.name }/>
             <Text h4 style={{ marginBottom: '0' }}>{ product.name }</Text>
-            <Text type="secondary" small className={ styles.truncate }>{ `${product.description} - ${product.size}` }</Text>
+            <Text small className={ styles.truncate }>{ `${product.description} - ${product.size}` }</Text>
             <Text h4 style={{ color: "#ea4335" }}>{ product.price }$</Text>
         </Card>
     </Link>
