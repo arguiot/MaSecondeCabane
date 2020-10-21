d5511b75cdf95d2c4bf85bd7dbde83f439c1d179
Arthur Guiot <arguiot@gmail.com>
2020-10-21 12:05:40 -0400

    Fixed typos
    Signed-off-by: Arthur Guiot <arguiot@gmail.com>

--- pages/about.js
+++ pages/about.js
@@ -19,10 +19,10 @@ export default function About() {
         <Text h1 align="center" className={ styles.title }>À propos de nous</Text>
         <Divider />
         <Text h4>
-            Chez la Seconde Cabane, nous proposons de donner une nouvelle vie aux articles de vos bambins!
+            Chez ma Seconde Cabane, nous proposons de donner une seconde vie aux articles de vos bambins!
         </Text>
         <Text p align="justify">
-            Acheter en seconde main ne veut pas dire de mauvaise qualité, au contraire ! Des vêtements encore neufs, ou peut utilisé ou encore en parfaite condition, nous en avons tous dans les penderies de nos enfants. Imaginez qu'entre 0 et 1 an, les vêtements ne sont portés que quelques semaines... Créer un vide dressing chic et de qualité pour que nous puissions partager tous ensemble nos trésors.
+            Acheter en seconde main ne veut pas dire de mauvaise qualité, au contraire ! Des vêtements encore neufs, ou peut utilisés ou encore en parfaite condition, nous en avons tous dans les penderies de nos enfants. Imaginez qu'entre 0 et 1 an, les vêtements ne sont portés que quelques semaines... Créer un vide dressing chic et de qualité pour que nous puissions partager tous ensemble nos trésors.
         </Text>
         <Text h4>Comment ça marche?</Text>
         <Text p align="justify">
