8ec0557070d5021ccc746b89e15645dc4e05378d
Arthur Guiot <arguiot@gmail.com>
2020-10-22 12:41:48 -0400

    Fixed product card
    Signed-off-by: Arthur Guiot <arguiot@gmail.com>

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,14 +1,22 @@
 import { Card, Image, Text, Row } from "@geist-ui/react"
+import { useRouter } from "next/router"
 import styles from "../styles/ProductCard.module.scss"
-import Link from 'next/link'
 export default function ProductCard({ product }) {
-    return <Link href="/product/[product]" as={ `/product/${product._id}` }>
+    const router = useRouter()
+    const redirect = e => {
+        e.preventDefault()
+        router.push(e.currentTarget.href).then(() => {
+            window.scrollTo(0, 0)
+            document.body.focus()
+        })
+    }
+    return <a href={ `/product/${product._id}` } onClick={redirect}>
         <Card width="300" hoverable shadow style={{ cursor: "pointer", textAlign: "center" }}>
             <Image width={ 300 } height={ 200 } src={ `https://ik.imagekit.io/ittx2e0v7x/tr:w-300,h-200,fo-auto/${product.image}` } style={{ objectFit: 'cover' }} alt={ product.name }/>
             <Text h4 style={{ marginBottom: '0' }}>{ product.name }</Text>
             <Text small className={ styles.truncate }>{ `${product.description} - ${product.size}` }</Text>
             <Text h4 style={{ color: "#ea4335" }}>{ product.price }$</Text>
         </Card>
-    </Link>
+    </a>
     
 }
--- pages/index.js
+++ pages/index.js
@@ -125,7 +125,7 @@ function Home({ products, router }) {
 			{
 				shuffle(products.filter(e => (e.quantity >= 1 && e.favorite == true))).map(p => {
 					return <Grid key={ p._id } xs={24} md={8}>
-						<ProductCard product={ p } />
+						<ProductCard key={ p._id } product={ p } />
 					</Grid>
 				})
 			}
