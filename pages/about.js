920422f24e76c471b6c580a3b3b7985b434915f3
Arthur Guiot <arguiot@gmail.com>
2020-10-27 11:11:10 -0400

    Fixed typo

--- pages/about.js
+++ pages/about.js
@@ -22,7 +22,7 @@ export default function About() {
             Chez ma Seconde Cabane, nous proposons de donner une seconde vie aux articles de vos bambins!
         </Text>
         <Text p align="justify">
-            Acheter en seconde main ne veut pas dire de mauvaise qualité, au contraire ! Des vêtements encore neufs, ou peut utilisés ou encore en parfaite condition, nous en avons tous dans les penderies de nos enfants. Imaginez qu'entre 0 et 1 an, les vêtements ne sont portés que quelques semaines... Créer un vide dressing chic et de qualité pour que nous puissions partager tous ensemble nos trésors.
+            Acheter en seconde main ne veut pas dire de mauvaise qualité, au contraire ! Des vêtements encore neufs, ou peu utilisés ou encore en parfaite condition, nous en avons tous dans les penderies de nos enfants. Imaginez qu'entre 0 et 1 an, les vêtements ne sont portés que quelques semaines... Créer un vide dressing chic et de qualité pour que nous puissions partager tous ensemble nos trésors.
         </Text>
         <Text h4>Comment ça marche?</Text>
         <Text p align="justify">
