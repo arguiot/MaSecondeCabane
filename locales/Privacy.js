62e0b568be7f6fab14d24710d6488954b034d350
Arthur Guiot <arguiot@gmail.com>
2020-10-29 15:52:17 -0400

    Translated privacy page

---
+++ locales/Privacy.js
@@ -0,0 +1,70 @@
+export default {
+    privacy: {
+        fr: "Confidentialité",
+        en: "Privacy"
+    },
+    subtitle: {
+        fr: "La protection de votre vie privée est importante pour nous. C’est la raison pour laquelle nous avons élaboré un Engagement de confidentialité qui régit la manière dont nous recueillons, utilisons, divulguons, transférons et stockons vos données personnelles.",
+        en: "Protecting your privacy is important to us. That's why we have developed a Privacy Policy that covers how we collect, use, disclose, transfer and store your personal data."
+    },
+    collect: {
+        fr: "Collecte et utilisation des données personnelles",
+        en: "Collection and use of personal data"
+    },
+    collectContentP1: {
+        fr: "Par données personnelles, nous entendons toutes les informations pouvant être utilisées pour identifier ou contacter une personne spécifique.",
+        en: "By personal data, we mean any information that can be used to identify or contact a specific person."
+    },
+    collectContentP2: {
+        fr: "Bien que nous essayons a tout prix de limiter la quantité d'informations que nous disposons sur nos utilisateurs, certaines données personnelles sont indispensables au bon fonctionnement du site internet.",
+        en: "Although we try at all costs to limit the amount of information we have about our users, some personal data is essential for the proper functioning of the website."
+    },
+    collectContentP3: {
+        fr: "Voici quelques exemples de types de données personnelles que nous receuillons et comment nous pouvons les utiliser:",
+        en: "Here are some examples of the types of personal information we receive and how we may use it:"
+    },
+    personalData: {
+        fr: "Données personnelles que nous recueillons:",
+        en: "Personal data we collect:"
+    },
+    whenYouBuy: {
+        fr: "Lorsque vous vendez ou achetez un produit, nous pouvons collecter tout un ensemble d’informations, telles que votre nom, votre adresse postale, votre numéro de téléphone, votre adresse e-mail, les données de votre carte bancaire.",
+        en: "When you sell or buy a product, we may collect a variety of information, such as your name, mailing address, telephone number, e-mail address, credit card information."
+    },
+    whenYouVisit: {
+        fr: "Lorsque vous visiter ce site internet, nous collectons anonymement certaines données visant à l'amélioration du site tel que votre adresse IP, votre localisation approximative ainsi que d'autres informations sur votre comportement lors de votre visite.",
+        en: "When you visit this website, we anonymously collect certain data aimed at improving the site such as your IP address, your approximate location and other information about your behaviour during your visit."
+    },
+    personalDataUsage: {
+        fr: "Utilisation de vos données personnelles",
+        en: "Use of your personal data"
+    },
+    personalDataUsageSubtitle: {
+        fr: "Nous pouvons traiter vos données personnelles dans les objectifs décrits dans cet Engagement de confidentialité avec votre consentement, pour nous conformer à une obligation légale à laquelle nous sommes soumis.",
+        en: "We may process your personal data for the purposes described in this Privacy Policy with your consent, to comply with a legal obligation to which we are subject."
+    },
+    newsletter: {
+        fr: "Les données personnelles que nous recueillons nous permettent de vous informer des dernières annonces produits et des mises à jours de notre service. Si vous ne souhaitez pas figurer sur notre liste de diffusion, vous pouvez vous y opposer à tout moment en mettant à jour vos préférences via notre partenaire.",
+        en: "The personal data we collect allows us to inform you of the latest product announcements and updates to our service. If you do not wish to be on our mailing list, you can oppose it at any time by updating your preferences via our partner."
+    },
+    improve: {
+        fr: "Nous utilisons également vos données personnelles pour créer, développer, utiliser, livrer et améliorer nos produits, services, contenus et publicités, et à des fins de prévention des pertes et de lutte contre la fraude. Nous pouvons aussi utiliser vos données personnelles dans des objectifs de sécurité des comptes et réseaux, notamment afin de protéger nos services pour le bénéfice de tous nos utilisateurs, ainsi que filtrer et analyser tout contenu chargé pour nous assurer qu’il ne contient pas de contenus illégaux. Lorsque nous utilisons vos données à des fins de lutte contre la fraude, c’est suite à une transaction en ligne auprès de nous. Nous limitons notre utilisation des données à des fins de lutte contre la fraude aux données strictement nécessaires et dans le cadre de nos intérêts légitimes estimés afin de protéger nos clients et nos services. Pour certaines transactions en ligne, nous pouvons également vérifier les informations que vous nous avez fournies auprès de sources publiquement disponibles.",
+        en: "We also use your personal data to create, develop, use, deliver and improve our products, services, content and advertising, and for loss prevention and anti-fraud purposes. We may also use your personal data for account and network security purposes, including to protect our services for the benefit of all our users, and to filter and analyze any uploaded content to ensure that it does not contain illegal content. When we use your data for anti-fraud purposes, it is as a result of an online transaction with us. We limit our use of data for anti-fraud purposes to that which is strictly necessary and in our legitimate interests to protect our customers and our services. For certain online transactions, we may also verify the information you have provided to us with publicly available sources."
+    },
+    internal: {
+        fr: "Nous pouvons également utiliser les données personnelles à des fins internes, par exemple pour des audits, analyses de données et recherches dans le but d’améliorer les produits, services et communications clients.",
+        en: "We may also use personal data for internal purposes, such as audits, data analysis and research to improve products, services and customer communications."
+    },
+    cookies: {
+        fr: "Cookies et autres technologies",
+        en: "Cookies and other technologies"
+    },
+    cookiesP1: {
+        fr: "Ce site web peut utiliser des « cookies » et d’autres technologies, telles que des « pixel tags » et des balises web. Ces technologies nous permettent de mieux comprendre le comportement des utilisateurs, nous indiquent quelles parties de nos sites web sont les plus visitées, et facilitent et mesurent l’efficacité des publicités et recherches Internet. Nous traitons les données collectées par les cookies et autres technologies comme des données non personnelles. Toutefois, si les adresses IP (Internet Protocol) ou des identifiants similaires sont considérés comme des données personnelles par la loi locale, nous traitons également ces identifiants comme des données personnelles. De la même manière, si des données non personnelles sont associées à des données personnelles, nous traitons les informations ainsi associées comme des données personnelles aux fins du présent Engagement de confidentialité.",
+        en: "This website may use \"cookies\" and other technologies, such as pixel tags and web beacons. These technologies allow us to better understand user behavior, tell us which parts of our websites are visited most frequently, and facilitate and measure the effectiveness of Internet advertising and research. We treat data collected by cookies and other technologies as non-personal data. However, if Internet Protocol (IP) addresses or similar identifiers are considered personal data by local law, we also treat such identifiers as personal data. Similarly, if non-personal data is associated with personal data, we treat the information so associated as personal data for the purposes of this Privacy Policy."
+    },
+    cookiesP2: {
+        fr: "Nous utilisons également des cookies et d’autres technologies pour se souvenir de vos données personnelles lorsque vous utilisez un site web, des services en ligne et des applications. Dans ce cas, notre objectif est de rendre votre visite plus pratique et de la personnaliser.",
+        en: "We also use cookies and other technologies to remember your personal data when you use a website, online services and applications. In these cases, our goal is to make your visit more convenient and personalized."
+    }
+}
--- pages/privacy.js
+++ pages/privacy.js
@@ -4,10 +4,10 @@ import Footer from "../components/Footer"
 import NavBar from "../components/NavBar"
 import { User } from '@geist-ui/react-icons'
 
-export default function Privacy() {
+export default function Privacy({ t }) {
     return <>
 	<Head>
-		<title>Confidentialité</title>
+        <title>Ma Seconde Cabane - { t.privacy }</title>
 		<link rel="icon" href="/favicon.ico" />
 	</Head>
 	<NavBar />
@@ -15,60 +15,75 @@ export default function Privacy() {
         <Row justify="center">
             <User size={96} />
         </Row>
-        <Text h1 align="center">Confidentialité</Text>
+        <Text h1 align="center">{ t.privacy }</Text>
         <Text h4 align="center">
-            La protection de votre vie privée est importante pour nous. C’est la raison pour laquelle nous avons élaboré un Engagement de confidentialité qui régit la manière dont nous recueillons, utilisons, divulguons, transférons et stockons vos données personnelles.
+            { t.subtitle }
         </Text>
         <Divider />
-        <Text h3>Collecte et utilisation des données personnelles</Text>
+        <Text h3>{ t.collect }</Text>
         <Text p>
-            Par données personnelles, nous entendons toutes les informations pouvant être utilisées pour identifier ou contacter une personne spécifique.
+            { t.collectContentP1 }
             <br />
-            Bien que nous essayons a tout prix de limiter la quantité d'informations que nous disposons sur nos utilisateurs, certaines données personnelles sont indispensables au bon fonctionnement du site internet.
+            { t.collectContentP2 }
             <br />
-            Voici quelques exemples de types de données personnelles que nous receuillons et comment nous pouvons les utiliser :
+            { t.collectContentP3 }
         </Text>
-        <Text h4>Données personnelles que nous recueillons:</Text>
+        <Text h4>{ t.personalData }</Text>
         <ul>
             <li>
                 <Text p>
-                    Lorsque vous vendez ou achetez un produit, nous pouvons collecter tout un ensemble d’informations, telles que votre nom, votre adresse postale, votre numéro de téléphone, votre adresse e-mail, les données de votre carte bancaire.
+                    { t.whenYouBuy }
                 </Text>
             </li>
             <li>
                 <Text p>
-                    Lorsque vous visiter ce site internet, nous collectons anonymement certaines données visant à l'amélioration du site tel que votre adresse IP, votre localisation approximative ainsi que d'autres informations sur votre comportement lors de votre visite.
+                    { t.whenYouVisit }
                 </Text>
             </li>
         </ul>
-        <Text h4>Utilisation de vos données personnelles</Text>
+        <Text h4>{ t.personalDataUsage }</Text>
         <Text p>
-            Nous pouvons traiter vos données personnelles dans les objectifs décrits dans cet Engagement de confidentialité avec votre consentement, pour nous conformer à une obligation légale à laquelle nous sommes soumis.
+            { t.personalDataUsageSubtitle }
         </Text>
         <ul>
             <li>
                 <Text p>
-                    Les données personnelles que nous recueillons nous permettent de vous informer des dernières annonces produits et des mises à jours de notre service. Si vous ne souhaitez pas figurer sur notre liste de diffusion, vous pouvez vous y opposer à tout moment en mettant à jour vos préférences via notre partenaire.
+                    { t.newsletter }
                 </Text>
             </li>
             <li>
                 <Text p>
-                    Nous utilisons également vos données personnelles pour créer, développer, utiliser, livrer et améliorer nos produits, services, contenus et publicités, et à des fins de prévention des pertes et de lutte contre la fraude. Nous pouvons aussi utiliser vos données personnelles dans des objectifs de sécurité des comptes et réseaux, notamment afin de protéger nos services pour le bénéfice de tous nos utilisateurs, ainsi que filtrer et analyser tout contenu chargé pour nous assurer qu’il ne contient pas de contenus illégaux. Lorsque nous utilisons vos données à des fins de lutte contre la fraude, c’est suite à une transaction en ligne auprès de nous. Nous limitons notre utilisation des données à des fins de lutte contre la fraude aux données strictement nécessaires et dans le cadre de nos intérêts légitimes estimés afin de protéger nos clients et nos services. Pour certaines transactions en ligne, nous pouvons également vérifier les informations que vous nous avez fournies auprès de sources publiquement disponibles.
+                    { t.improve }
                 </Text>
             </li>
             <li>
                 <Text p>
-                    Nous pouvons également utiliser les données personnelles à des fins internes, par exemple pour des audits, analyses de données et recherches dans le but d’améliorer les produits, services et communications clients.
+                    { t.internal }
                 </Text>
             </li>
         </ul>
-        <Text h4>Cookies et autres technologies</Text>
+        <Text h4>{ t.cookies }</Text>
         <Text p>
-            Ce site web peut utiliser des « cookies » et d’autres technologies, telles que des « pixel tags » et des balises web. Ces technologies nous permettent de mieux comprendre le comportement des utilisateurs, nous indiquent quelles parties de nos sites web sont les plus visitées, et facilitent et mesurent l’efficacité des publicités et recherches Internet. Nous traitons les données collectées par les cookies et autres technologies comme des données non personnelles. Toutefois, si les adresses IP (Internet Protocol) ou des identifiants similaires sont considérés comme des données personnelles par la loi locale, nous traitons également ces identifiants comme des données personnelles. De la même manière, si des données non personnelles sont associées à des données personnelles, nous traitons les informations ainsi associées comme des données personnelles aux fins du présent Engagement de confidentialité.
+            { t.cookiesP1 }
             <br/>
-            Nous utilisons également des cookies et d’autres technologies pour se souvenir de vos données personnelles lorsque vous utilisez un site web, des services en ligne et des applications. Dans ce cas, notre objectif est de rendre votre visite plus pratique et de la personnaliser.
+            { t.cookiesP2 }
         </Text>
     </Page>
     <Footer />
     </>
+}
+
+import Locales from "../locales/Privacy"
+
+export async function getStaticProps({ locale }) {
+    // Locales
+	const locales = Object.fromEntries(Object.entries(Locales).map(line => [
+		line[0],
+		line[1][locale]
+    ]))
+    return {
+        props: {
+			t: locales
+        }
+    }
 }
