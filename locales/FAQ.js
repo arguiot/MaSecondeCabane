320b8303d37ff5133da1d10fa972925f18566bba
Arthur Guiot <arguiot@gmail.com>
2020-11-02 13:59:43 -0500

    Translated FAQ

--- locales/FAQ.js
+++ locales/FAQ.js
@@ -3,7 +3,15 @@ export default {
         fr: "FAQ",
         en: "Q&A"
     },
+    questions: {
+        fr: "Des questions?",
+        en: "Questions?"
+    },
     // Buy
+    buy: {
+        fr: "ACHETER SUR MA SECONDE CABANE",
+        en: "BUY ON MA SECONDE CABANE"
+    },
     questionsTitle: {
         fr: "J’ai des questions à propos de cet article, comment faire ?",
         en: "I have questions about this article, what can I do?"
@@ -39,5 +47,58 @@ export default {
     controlledContent: {
         fr: "Tous les articles mis en ligne sont minutieusement controlés par nos soins. En achetant sur le site, vous êtes certains de la grande qualité des produits. A ce propos, nous ne proposons que des articles neufs, en excellent ou en très bon état.",
         en: "All the articles put online are meticulously checked by us. By buying on the site, you are sure of the high quality of the products. By the way, we propose only new items, in excellent or very good condition."
+    },
+    // Sell
+    sell: {
+        fr: "VENDRE SUR MA SECONDE CABANE",
+        en: "SELL ON MA SECONDE CABANE"
+    },
+    whichItems: {
+        fr: "Quels articles peut on vendre sur le site ?",
+        en: "Which products can be sold on the site?"
+    },
+    weSellP1: {
+        fr: "Nous vendons des vêtements de marque pour bébés et enfants jusqu’à 10 ans. Tous les vêtements qui sont mis en vente sont neufs ou en parfait état. Tous les articles que vous nous confiez doivent être lavés et en excellent état : sans tâche, ni trou, ni bouloche. Si vous avez un doute sur les produits que vous souhaitez vendre, n’hésitez pas à nous envoyer un mail à",
+        en: "We sell branded clothing for babies and children up to 10 years old. All the clothes on sale are new or in perfect condition. All the items you entrust to us must be washed and in excellent condition: no stains, holes or pilling. If you have any doubt about the products you wish to sell, do not hesitate to send us an email at"
+    },
+    weSellP2: {
+        fr: ", nous nous ferons un plaisir de vous répondre. Pour avoir plus d’informations sur nos conditions de vente, nous vous invitons à consulter notre page",
+        en: ", we will be pleased to answer you. For more information on our terms and conditions of sale, please consult this page: "
+    },
+    sellPage: {
+        fr: "Vendre",
+        en: "Sell"
+    },
+    howToSell: {
+        fr: "Comment faire pour vendre mes articles ?",
+        en: "How do I sell my items?"
+    },
+    howToSellP1: {
+        fr: "Rien de plus simple ! Il vous suffit de cliquer",
+        en: "Nothing could be simpler! You just have to click"
+    },
+    here: {
+        fr: "ici",
+        en: "here"
+    },
+    howToSellP2: {
+        fr: "Pour les habitants de la région du grand Montréal: nous nous déplaçons directement chez vous. Prenez RDV via le formulaire et nous trouverons le moment qui convient pour venir chez vous. Pour le reste du Québec, nous travaillons à un système de collecte mais vous pouvez d'ores et déjà remplir le formulaire pour nous faire part de votre demande.",
+        en: "For residents of the Greater Montreal area: we come directly to your home. Make an appointment via the form and we will find the right time to come to your home. For the rest of Quebec, we are working on a collection system but you can already fill out the form to let us know about your request."
+    },
+    whenWillIBePayed: {
+        fr: "Quand serais-je payé ?",
+        en: "When will I get paid?"
+    },
+    whenWillIBePayedContent: {
+        fr: "Dans les jours suivant la réception de votre collecte, nous vous enverrons un mail avec la fiche récapitulative des vêtements que vous nous avez confiés, ainsi que la somme que nous devons vous régler.",
+        en: "In the days following the reception of your collection, we will send you an email with the summary sheet of the clothes you have entrusted to us, as well as the amount we have to pay you."
+    },
+    howWillIBePayed: {
+        fr: "Comment serais-je payé ?",
+        en: "How will I be paid?"
+    },
+    howWillIBePayedContent: {
+        fr: "Dès réception de vos coordonnées bancaires, un virement par Interac sera effectué sur votre compte bancaire.",
+        en: "Upon receipt of your banking information, an Interac transfer will be made to your bank account."
     }
 }
--- pages/faq.js
+++ pages/faq.js
@@ -7,10 +7,10 @@ import NavBar from "../components/NavBar"
 import NextLink from "next/link"
 import styles from "../styles/FAQ.module.scss"
 
-export default function About() {
+export default function Faq({ t }) {
     return <>
 	<Head>
-		<title>Ma Seconde Cabane - FAQ</title>
+		<title>Ma Seconde Cabane - { t.faq }</title>
 		<link rel="icon" href="/favicon.ico" />
 	</Head>
 	<NavBar />
@@ -20,42 +20,54 @@ export default function About() {
         </Row>
         <Text h1 align="center">Des questions?</Text>
         <Spacer y={3} />
-        <Text h3 style={{ fontFamily: "Quicksand" }}>ACHETER SUR MA SECONDE CABANE</Text>
+        <Text h3 style={{ fontFamily: "Quicksand" }}>{ t.buy }</Text>
         <Collapse.Group className={ styles.collapse }>
-        <Collapse title="J’ai des questions à propos d’un article mis en vente, comment faire ?">
-            Pour toutes vos questions concernant un article, veuillez envoyer un mail à l'adresse suivante : <Link href="mailto:contact@masecondecabane.com" color>contact@masecondecabane.com</Link>.
-            Nous nous ferons un plaisir de vous renseigner.
-        </Collapse>
-        <Collapse title="Quels sont les moyens de paiement acceptés ?">
-        Notre système de paiement est Stripe. Toutes les cartes de crédit et débit sont acceptés (à conditions que votre banque accepte les achats en ligne). Vous pouvez également utiliser Apple Pay et Google Pay pour passer à la caisse encore plus vite!
-        </Collapse>
-        <Collapse title="Quand vais je recevoir ma commande ?">
-            Votre commande est expédiée entre 24h et 72h selon le moment où vous passez votre commande. Les commandes enregistrées sur le site le vendredi après 12h, le samedi, le dimanche ou les jours fériés seront traitées le lundi suivant.
-            Malheureusement, nous ne sommes pas responsables des délais des transporteurs.
-            Les articles commandés seront livrés à l’adresse que vous avez indiqué.
-        </Collapse>
-        <Collapse title="Est-ce que les articles mis en ligne sont controlés avant d’etre envoyés ?">
-            Tous les articles mis en ligne sont minutieusement controlés par nos soins. En achetant sur le site, vous êtes certains de la grande qualité des produits. A ce propos, nous ne proposons que des articles neufs, en excellent ou en très bon état.
-        </Collapse>
+            <Collapse title={ t.questionsTitle }>
+                { t.questionsP1 } <Link href="mailto:contact@masecondecabane.com" color>contact@masecondecabane.com</Link>. { t.questionsP2 }
+            </Collapse>
+            <Collapse title={ t.paymentTitle }>
+                { t.paymentContent }
+            </Collapse>
+            <Collapse title={ t.whenArrive }>
+                { t.whenArriveContent }
+            </Collapse>
+            <Collapse title={ t.controlledTitle }>
+                { t.controlledContent }
+            </Collapse>
         </Collapse.Group>
         <Spacer y={3} />
-        <Text h3 style={{ fontFamily: "Quicksand" }}>VENDRE SUR MA SECONDE CABANE</Text>
+        <Text h3 style={{ fontFamily: "Quicksand" }}>{ t.sell }</Text>
         <Collapse.Group className={ styles.collapse }>
-        <Collapse title="Quels articles peut on vendre sur le site ?">
-        Nous vendons des vêtements de marque pour bébés et enfants jusqu’à 10 ans. Tous les vêtements qui sont mis en vente sont neufs ou en parfait état. Tous les articles que vous nous confiez doivent être lavés et en excellent état : sans tâche, ni trou, ni bouloche. Si vous avez un doute sur les produits que vous souhaitez vendre, n’hésitez pas à nous envoyer un mail à <Link href="mailto:contact@masecondecabane.com" color>contact@masecondecabane.com</Link>, nous nous ferons un plaisir de vous répondre. Pour avoir plus d’informations sur nos conditions de vente, nous vous invitons à consulter notre page <NextLink href="/product/sell"><Link color>Vendre</Link></NextLink>.
-        </Collapse>
-        <Collapse title="Comment faire pour vendre mes articles ?">
-        Rien de plus simple ! Il vous suffit de cliquer <NextLink href="/product/sell"><Link color>ici</Link></NextLink>.
-        Pour les habitants de la région du grand Montréal: nous nous déplaçons directement chez vous. Prenez RDV via le formulaire et nous trouverons le moment qui convient pour venir chez vous. Pour le reste du Québec, nous travaillons à un système de collecte mais vous pouvez d'ores et déjà remplir le formulaire pour nous faire part de votre demande.
-        </Collapse>
-        <Collapse title="Quand serais-je payé ?">
-            Dans les jours suivant la réception de votre collecte, nous vous enverrons un mail avec la fiche récapitulative des vêtements que vous nous avez confiés, ainsi que la somme que nous devons vous régler.
-        </Collapse>
-        <Collapse title="Comment serais-je payé ?">
-            Dès réception de vos coordonnées bancaires, un virement par interact sera effectué sur votre compte bancaire.
-        </Collapse>
+            <Collapse title={ t.whichItems }>
+                { t.weSellP1 } <Link href="mailto:contact@masecondecabane.com" color>contact@masecondecabane.com</Link>{ t.weSellP2 } <NextLink href="/product/sell"><Link color>{ t.sellPage }</Link></NextLink>.
+            </Collapse>
+            <Collapse title={ t.howToSell }>
+                { t.howToSellP1 } <NextLink href="/product/sell"><Link color>{ t.here }</Link></NextLink>. { t.howToSellP2 }
+            </Collapse>
+            <Collapse title={ t.whenWillIBePayed }>
+                { t.whenWillIBePayedContent }
+            </Collapse>
+            <Collapse title={ t.howWillIBePayed }>
+                { t.howWillIBePayedContent }
+            </Collapse>
         </Collapse.Group>
     </Page>
     <Footer />
     </>
+}
+
+import Locales from "../locales/FAQ"
+
+export async function getStaticProps({ locale }) {
+    // Locales
+	const locales = Object.fromEntries(Object.entries(Locales).map(line => [
+		line[0],
+		line[1][locale.split("-")[0]]
+    ]))
+
+    return {
+        props: {
+            t: locales
+        }
+    }
 }
