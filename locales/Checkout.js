38ccf650b1251a85f486af0b4bbdb84708905630
Arthur Guiot <arguiot@gmail.com>
2020-11-03 15:32:04 -0500

    Translated checkout

---
+++ locales/Checkout.js
@@ -0,0 +1,22 @@
+export default {
+    thanks: {
+        fr: "Merci!",
+        en: "Thank you!"
+    },
+    receive: {
+        fr: "Vous recevrez votre commande d'ici peu.",
+        en: "You will receive your order shortly."
+    },
+    oups: {
+        fr: "Oups!",
+        en: "Oops!"
+    },
+    problem: {
+        fr: "Un problème est survenu! Réessayez, vous aurez peut-être plus de chance.",
+        en: "A problem has occurred! Try again, you might have better luck."
+    },
+    back: {
+        fr: "Retour à la case départ",
+        en: "Back to the start"
+    }
+}
