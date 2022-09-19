837788ff32622136f11517d4f06b0bfd5962ab04
Arthur Guiot <arguiot@gmail.com>
2022-09-19 19:15:16 -0400

    Trying not parsing intent

--- pages/api/pos/capture_payment_intent.js
+++ pages/api/pos/capture_payment_intent.js
@@ -1,7 +1,7 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
 
 export default async (req, res) => {
-    const body = JSON.parse(req.body)
+    const body = req.body // Body is a JSON object
     const intent = await stripe.paymentIntents.capture(body.payment_intent_id);
     res.status(200).json({ intent });
 }
