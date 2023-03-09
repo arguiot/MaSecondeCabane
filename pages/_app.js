13b468406dcc8d875734a1a28eca15b0c8d662aa
Arthur Guiot <arguiot@gmail.com>
2023-03-09 15:32:05 +0000

    Made config more robust

--- pages/_app.js
+++ pages/_app.js
@@ -1,84 +1,103 @@
-import '../styles/globals.scss'
-import {
-	GeistProvider,
-	CssBaseline,
-	useTheme
-} from '@geist-ui/react'
-import NextNProgress from 'nextjs-progressbar';
+import "../styles/globals.scss";
+import { GeistProvider, CssBaseline, useTheme } from "@geist-ui/react";
+import NextNProgress from "nextjs-progressbar";
 import React from "react";
-import { FilterContextProvider } from '../components/FilterContext';
-import Promo from '../components/Promo'
+import { FilterContextProvider } from "../components/FilterContext";
+import Promo from "../components/Promo";
+import getConfig from "../lib/config";
 
 // Config context + hook
 const defaultConfig = {
-	locked: false,
-	popUpAddress: {
-		street: "5000 rue d'Iberville, Suite 320",
-		city: "Montreal",
-		country: "CA, QC",
-		zipCode: "H2H 2S6"
-	},
-	freeShipping: 90,
-	promo: {
-		fr: "Livraison gratuite pour les commandes de plus de XX$",
-		en: "Free shipping for orders over $XX"
-	}
-}
-export const Config = React.createContext(defaultConfig)
+    locked: false,
+    popUpAddress: {
+        street: "5000 rue d'Iberville, Suite 320",
+        city: "Montreal",
+        country: "CA, QC",
+        zipCode: "H2H 2S6",
+    },
+    freeShipping: 90,
+    promo: {
+        fr: "Livraison gratuite pour les commandes de plus de XX$",
+        en: "Free shipping for orders over $XX",
+    },
+};
+export const Config = React.createContext(defaultConfig);
 
 export function useConfig() {
-	return React.useContext(Config)
+    return React.useContext(Config);
 }
 
-function MyApp({
-	Component,
-	pageProps
-}) {
-	const [themeType, setThemeType] = React.useState('light')
-	React.useEffect(() => {
-		const md = window.matchMedia('(prefers-color-scheme: dark)')
-		setThemeType(lastThemeType => (md.matches ? 'dark' : 'light'))
+function MyApp({ Component, pageProps }) {
+    const [themeType, setThemeType] = React.useState("light");
+    const [passedConfig, setPassedConfig] = React.useState(defaultConfig);
 
-		const handler = event => {
-			setThemeType(lastThemeType => (event.matches ? 'dark' : 'light'))
-		}
+    React.useEffect(() => {
+        // Theme
+        const md = window.matchMedia("(prefers-color-scheme: dark)");
+        const updateThemeType = (event) => {
+            setThemeType(event.matches ? "dark" : "light");
+        };
+        updateThemeType(md);
+        md.addEventListener("change", updateThemeType);
 
-		try {
-			// Chrome & Firefox
-			md.addEventListener('change', handler);
-		} catch (e1) {
-			try {
-				// Safari
-				md.addListener(handler);
-			} catch (e2) {
-				console.error(e2);
-			}
-		}
-	}, [])
+        // Config
+        const config = JSON.parse(localStorage.getItem("config") || "{}");
+        const expiration = config.expiration || 0;
 
-	const theme = useTheme()
+        const setConfig = async (newConfig) => {
+            localStorage.setItem(
+                "config",
+                JSON.stringify({
+                    ...newConfig,
+                    expiration: Date.now() + 1000 * 60 * 60 * 24,
+                })
+            );
+            setPassedConfig(newConfig);
+        };
 
-	const passedConfig = pageProps.config || defaultConfig
+        const updateConfig = async () => {
+            if (expiration > Date.now()) {
+                setPassedConfig(config);
+            } else {
+                const newConfig = await getConfig();
+                setConfig(newConfig);
+            }
+        };
 
-	return (
-	<GeistProvider theme={{
-		type: themeType,
-		palette: {
-			foreground: "#007577"
-		}
-		}}>
-		<FilterContextProvider>
-			<Config.Provider value={passedConfig}>
-				<CssBaseline />
-				<Component {...pageProps} />
-				<NextNProgress color="var(--text-color)" />
-				<Promo />
-			</Config.Provider>
-		</FilterContextProvider>
-	</GeistProvider>
-	)
+        if (pageProps.config) {
+            setConfig(pageProps.config);
+        } else {
+            updateConfig();
+        }
+
+        return () => {
+            md.removeEventListener("change", updateThemeType);
+        };
+    }, [pageProps.config]);
+
+    const theme = useTheme();
+
+    return (
+        <GeistProvider
+            theme={{
+                type: themeType,
+                palette: {
+                    foreground: "#007577",
+                },
+            }}
+        >
+            <FilterContextProvider>
+                <Config.Provider value={passedConfig}>
+                    <CssBaseline />
+                    <Component {...pageProps} />
+                    <NextNProgress color="var(--text-color)" />
+                    <Promo />
+                </Config.Provider>
+            </FilterContextProvider>
+        </GeistProvider>
+    );
 }
-export default MyApp
+export default MyApp;
 
 // export function reportWebVitals({
 // 	id,
