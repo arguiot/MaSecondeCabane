e8107125c934832f8ac25fcfe160e178a823823a
Arthur Guiot <arguiot@gmail.com>
2023-12-12 10:58:45 +0100

    Updated schema

--- db/schema.ts
+++ db/schema.ts
@@ -1,10 +1,11 @@
-import { index, int, text, varchar, mysqlTable, boolean, bigint, float, timestamp, json, serial } from 'drizzle-orm/mysql-core';
+import { index, int, text, varchar, mysqlTable, boolean, float, json } from 'drizzle-orm/mysql-core';
 import { moment } from './types/moment';
 import { ID, dID } from './types/ID';
+import { relations } from 'drizzle-orm';
 
 export const products = mysqlTable('Products', {
     _id: ID('id').autoincrement().primaryKey(),
-    _ts: moment('ts').notNull(),
+    _ts: moment('ts').notNull().$defaultFn(() => new Date().getTime()),
     name: varchar('name', { length: 255 }).notNull(),
     description: text('description').notNull(),
     descriptionEn: text('descriptionEn'),
@@ -53,9 +54,16 @@ export const customer = mysqlTable('Customer', {
     }
 });
 
+export const customerRelation = relations(customer, ({ one, many }) => ({
+    address: one(address, {
+        fields: [customer.addressId],
+        references: [address._id],
+    }),
+}));
+
 export const request = mysqlTable('Request', {
     _id: ID('id').autoincrement().primaryKey(),
-    _ts: moment('ts').notNull(),
+    _ts: moment('ts').notNull().$defaultFn(() => new Date().getTime()),
     customerId: dID('customerId').references(() => customer._id).notNull(),
     description: text('description').notNull(),
     done: boolean('done'),
@@ -65,9 +73,16 @@ export const request = mysqlTable('Request', {
     }
 });
 
+export const requestRelation = relations(request, ({ one, many }) => ({
+    customer: one(customer, {
+        fields: [request.customerId],
+        references: [customer._id],
+    }),
+}));
+
 export const order = mysqlTable('Order', {
     _id: ID('id').autoincrement().primaryKey(),
-    _ts: moment('ts').notNull(),
+    _ts: moment('ts').notNull().$defaultFn(() => new Date().getTime()),
     customerId: dID('customerId').references(() => customer._id).notNull(),
     stripeID: varchar('stripeID', { length: 255 }).notNull(),
     total: float('total').notNull(),
@@ -78,6 +93,14 @@ export const order = mysqlTable('Order', {
         orderIdx: index("order_idx").on(table._id),
     }
 });
+
+export const orderRelation = relations(order, ({ one, many }) => ({
+    customer: one(customer, {
+        fields: [order.customerId],
+        references: [customer._id],
+    }),
+    line: many(orderProductLine),
+}));
 
 export const orderProductLine = mysqlTable('OrderProductLine', {
     _id: ID('id').autoincrement().primaryKey(),
@@ -90,6 +113,17 @@ export const orderProductLine = mysqlTable('OrderProductLine', {
     }
 });
 
+export const orderProductLineRelation = relations(orderProductLine, ({ one, many }) => ({
+    order: one(order, {
+        fields: [orderProductLine.orderId],
+        references: [order._id],
+    }),
+    product: one(products, {
+        fields: [orderProductLine.productId],
+        references: [products._id],
+    }),
+}));
+
 export const websiteConfig = mysqlTable('WebsiteConfig', {
     _id: ID('id').autoincrement().primaryKey(),
     locked: boolean('locked').notNull(),
@@ -99,3 +133,10 @@ export const websiteConfig = mysqlTable('WebsiteConfig', {
     promoFr: varchar('promoFr', { length: 255 }).notNull(),
     promoEn: varchar('promoEn', { length: 255 }).notNull(),
 });
+
+export const websiteConfigRelation = relations(websiteConfig, ({ one, many }) => ({
+    popUpAddress: one(address, {
+        fields: [websiteConfig.popUpAddressId],
+        references: [address._id],
+    }),
+}));
