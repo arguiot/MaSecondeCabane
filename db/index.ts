97f92eec74849eefcd2e1c56afdbd47e0d9edd95
Arthur Guiot <arguiot@gmail.com>
2024-04-06 20:46:35 -0700

    New DB connection

--- db/index.ts
+++ db/index.ts
@@ -1,6 +1,20 @@
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
-import * as schema from "./schema";
-// create the connection
-const sql = neon(process.env.DRIZZLE_DATABASE_URL!);
-export const db = drizzle(sql, { schema });
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from './schema';
+
+const user = process.env.PGUSER;
+const password = process.env.PGPASSWORD;
+const host = process.env.PGHOST;
+const port = 5432;
+const database = process.env.PGPASSWORD;
+
+const queryClient = postgres("postgres://username:password@host.com:5432/database", {
+    host,
+    port: parseInt(port || "5432"),
+    database,
+    username: user,
+    password,
+    ssl: true
+});
+
+export const db = drizzle(queryClient, { schema });
