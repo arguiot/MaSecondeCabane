3c4f36d1ad8d2f4e28086cb7774e823c33f6db69
Arthur Guiot <arguiot@gmail.com>
2024-04-06 20:48:45 -0700

    Updated webpack config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@ module.exports = {
     env: {
         STRIPE_PUBLIC: process.env.STRIPE_PUBLIC,
     },
+    webpack: (config) => {
+        config.resolve.fallback = { fs: false };
+        config.externals.push(
+            "pino-pretty",
+            "lokijs",
+            "encoding",
+            "bufferutil",
+            "utf-8-validate",
+        );
+
+        return config;
+    },
     i18n: {
         locales: ['en-CA', 'fr-CA'],
         defaultLocale: 'fr-CA',
